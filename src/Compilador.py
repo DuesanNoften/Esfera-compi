54630ab97492c3d8a1713909c5b701c68623682d
DuesanNoften <alejandre030902@gmail.com>
2023-03-15 23:49:09 -0600

    Se creo el archivo compilador.py y se realizaron los primeros esbozos de sintaxis

---
+++ src/Compilador.py
@@ -0,0 +1,144 @@
+import sys
+sys.path.insert(0, "../..")
+
+if sys.version_info[0] >= 3:
+    raw_input = input
+
+reserved = {
+    'Proc':'PROC',
+    }
+
+tokens = [
+    'NAME', 'NUMBER', 'BOOL','COMMENT',
+    
+] + list(reserved.values())
+
+literals = ['=', '+', '-', '*', '/', '(', ')', ',']
+
+# Tokens
+t_DEF = '[Def]+'
+t_COMMENT = '[--][a-zA-Z0-9_#$%&/()=!"?\¡¿+~}`{^;,:.@°|¬-]*'
+t_NAME = r'[@][a-zA-Z0-9_#]*'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_BOOL(t):
+    r'\d+'
+    t.value = bool(t.value)
+    return t
+
+t_ignore = " \t"
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lex.lex()
+
+# Parsing rules
+
+precedence = (
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('right', 'UMINUS'),
+)
+
+# dictionary of names
+names = {}
+procs = {}
+
+
+def p_statement_assign(p):
+    'statement : DEF "(" NAME "," expression ")"'
+    if len(p[3])>1 and len(p[3])<=10 :
+        names[p[3]] = p[5]
+    else:
+        print("Las variables deben constar de minimo 3 caracteres y maximo 10 \ncontando con el @")
+
+#def p_statement_proc(p):
+#    'statement :  NAME "(" expression ")"'
+#    if len(p[3])>1 and len(p[3])<10 :
+#        procs[p[2]+p[3]] = p[4]+p[5]+p[6]
+#    else:
+#        print("Las variables deben constar de minimo 3 caracteres y maximo 10 \ncontando con el @")
+
+def p_statement_comment(p):
+    'statement : COMMENT '
+    
+    
+def p_statement_expr(p):
+    'statement : expression'
+    print(p[1])
+
+
+def p_expression_binop(p):
+    '''expression : expression '+' expression
+                  | expression '-' expression
+                  | expression '*' expression
+                  | expression '/' expression'''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+
+
+def p_expression_uminus(p):
+    "expression : '-' expression %prec UMINUS"
+    p[0] = -p[2]
+
+
+def p_expression_group(p):
+    "expression : '(' expression ')'"
+    p[0] = p[2]
+
+
+def p_expression_number(p):
+    "expression : NUMBER"
+    p[0] = p[1]
+
+def p_expression_bool(p):
+    "expression : BOOL"
+    p[0] = p[1]
+
+
+def p_expression_name(p):
+    "expression : NAME"
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = 0
+        
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+import ply.yacc as yacc
+yacc.yacc()
+
+while 1:
+    try:
+        s = raw_input('Esfera-Compi > ')
+    except EOFError:
+        break
+    if not s:
+        continue
+    yacc.parse(s)
